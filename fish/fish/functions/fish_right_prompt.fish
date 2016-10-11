ace62b0acd98687929793543efafed391057d260
Evan Peterson <77evan@gmail.com>
2016-10-10 22:47:04 -0400

    Fixed fish prompt git status colors

--- fish/fish/functions/fish_right_prompt.fish
+++ fish/fish/functions/fish_right_prompt.fish
@@ -14,16 +14,45 @@ end
 
 function fish_right_prompt
     set -l prompt_char "❮"
-    #"echo "$prompt_char"
-
-    set -l color_git $fish_color_command
-    if is_git_dirty
-        set color_git $fish_color_error
-    end
 
    	if is_git
-    	set_color $color_git
-    	echo -n $prompt_char" "(git_branch)" "
+
+        set -l color_git $fish_color_command
+        #if is_git_dirty
+        #    set color_git $fish_color_error
+        #end
+
+        set -l color_untracked normal
+        set -l color_modified normal
+        set -l color_staged normal
+        set -l git_status 0
+
+        if not git diff --cached --quiet 2> /dev/null
+            set color_staged $fish_color_command
+            set git_status 1
+        end
+        if not test -z (git ls-files-root --other --exclude-standard 2> /dev/null)
+            set color_untracked $fish_color_command
+            set git_status 2
+        end
+        if not git diff --quiet 2> /dev/null
+            set color_modified $fish_color_command
+            set git_status 2
+        end
+
+        switch $git_status
+        case 0
+            set color_git green
+        case 1
+            set color_git yellow
+        case 2
+            set color_git red
+        end
+
+        echo -n (set_color $color_staged)$prompt_char
+        echo -n (set_color $color_modified)$prompt_char
+        echo -n (set_color $color_untracked)$prompt_char
+    	echo -n (set_color $color_git)" "(git_branch)" "
     	set_color normal
   	end 
 end
