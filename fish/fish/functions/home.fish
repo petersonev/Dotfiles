d3c67963287a11b48c18eb80b602de1bc9822de4
Evan Peterson <77evan@gmail.com>
2016-10-13 00:04:32 -0400

    Fix fish home attach

--- fish/fish/functions/home.fish
+++ fish/fish/functions/home.fish
@@ -1,7 +1,7 @@
 function home
     switch (echo $argv)
     case attach
-        ssh -p 9000 -t petersonev.com 'LANG=en_CA.utf8 exec tmux new -A -s ssh'
+        ssh -p 9000 -t petersonev.com 'tmux new -A -s ssh'
     case lftp
         lftp -p 9000 sftp://petersonev.com
     case '*'
