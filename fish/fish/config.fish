2f36c26cc2fca4544a838ac730efd51504ee628f
Evan Peterson <77evan@gmail.com>
2016-11-02 13:44:48 -0400

    OS specific paths

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -1,5 +1,14 @@
 # This is fish config
 
+switch (uname -s)
+    case "Darwin"
+        # Used for macOS
+        set PATH $PATH /usr/local/sbin
+    case "Linux"
+        # TODO: seperate linux distros
+        set PATH /usr/bin/core_perl $PATH
+end
+
 set fish_greeting ""
 #set LANG en_US.UTF-8
 
